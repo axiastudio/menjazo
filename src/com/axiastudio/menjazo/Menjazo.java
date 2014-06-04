557653fc50a2e5b079711c82f2216df9edfd07af
Tiziano Lattisi <tiziano@axiastudio.it>
2014-06-04 19:26:19 +0200

    1st (void) commit

---
+++ src/com/axiastudio/menjazo/Menjazo.java
@@ -0,0 +1,13 @@
+package com.axiastudio.menjazo;
+
+/**
+ * User: tiziano
+ * Date: 04/06/14
+ * Time: 19:24
+ */
+public class Menjazo {
+
+    public static Menjazo create() {
+        return new Menjazo();
+    }
+}
